b957a5543b0ef8c5faaa0a71370a3daed624dfe8
Jeff Langr <Jeff@Langrsoft.com>
2018-11-09 15:34:05 -0700

    no semis

--- src/misc/name-normalizer.js
+++ src/misc/name-normalizer.js
@@ -1,3 +1,3 @@
 export const normalize = _ => {
-  return 'wha?';
-};
+  return 'wha?'
+}
--- src/misc/name-normalizer.test.js
+++ src/misc/name-normalizer.test.js
@@ -1,40 +1,40 @@
-import { expect } from 'chai';
-import { normalize } from './name-normalizer';
+import { expect } from 'chai'
+import { normalize } from './name-normalizer'
 
 describe('a name normalizer', () => {
   xit('returns empty when passed empty string', () => {
-    expect(normalize('')).to.equal('');
-  });
+    expect(normalize('')).to.equal('')
+  })
 
   xit('returns single word name', () => {
-    expect(normalize('Plato')).to.equal('Plato');
-  });
+    expect(normalize('Plato')).to.equal('Plato')
+  })
 
   xit('swaps first and last names', () => {
-    expect(normalize('Haruki Murakami')).to.equal('Murakami, Haruki');
-  });
+    expect(normalize('Haruki Murakami')).to.equal('Murakami, Haruki')
+  })
 
   xit('trims leading and trailing whitespace', () => {
-    expect(normalize('  Big Boi   ')).to.equal('Boi, Big');
-  });
+    expect(normalize('  Big Boi   ')).to.equal('Boi, Big')
+  })
 
   xit('initializes middle name', () => {
-    expect(normalize('Henry David Thoreau')).to.equal('Thoreau, Henry D.');
-  });
+    expect(normalize('Henry David Thoreau')).to.equal('Thoreau, Henry D.')
+  })
 
   xit('does not initialize one letter middle name', () => {
-    expect(normalize('Harry S Truman')).to.equal('Truman, Harry S');
-  });
+    expect(normalize('Harry S Truman')).to.equal('Truman, Harry S')
+  })
 
   xit('initializes each of multiple middle names', () => {
-    expect(normalize('Julia Scarlett Elizabeth Louis-Dreyfus')).to.equal('Louis-Dreyfus, Julia S. E.');
-  });
+    expect(normalize('Julia Scarlett Elizabeth Louis-Dreyfus')).to.equal('Louis-Dreyfus, Julia S. E.')
+  })
 
   xit('appends suffixes to end', () => {
-    expect(normalize('Martin Luther King, Jr.')).to.equal('King, Martin L., Jr.');
-  });
+    expect(normalize('Martin Luther King, Jr.')).to.equal('King, Martin L., Jr.')
+  })
 
   xit('throws when name contains two commas', () => {
-    expect(() => { normalize('Thurston, Howell, III'); }).to.throw();
-  });
-});
+    expect(() => { normalize('Thurston, Howell, III'); }).to.throw()
+  })
+})
