52a632caf4d43610d1ad4ae34d83702d11dc4336
Jeff Langr <Jeff@Langrsoft.com>
2018-11-09 15:17:44 -0700

    functional portfolio solution

---
+++ src/misc/portfolio.js
@@ -0,0 +1,19 @@
+export const createPortfolio = () => ({ holdings: {} })
+
+export let symbolLookup = _symbol => { throw new Error('not yet implemented') }
+export const symbolLookupStub = stub => symbolLookup = stub
+
+export const empty = portfolio => size(portfolio) === 0
+
+export const size = portfolio => Object.keys(portfolio.holdings).length
+
+export const value = portfolio => empty(portfolio) ? 0 :
+  Object.keys(portfolio.holdings).reduce(
+    (totalValue, symbol) => totalValue + symbolLookup(symbol) * sharesOf(portfolio, symbol),
+    0)
+
+export const sharesOf = (portfolio, symbol) =>
+  !portfolio.holdings[symbol] ? 0 : portfolio.holdings[symbol]
+
+export const purchase = (portfolio, symbol, shares) =>
+  ({ ...portfolio, holdings: { ...portfolio.holdings, [symbol]: sharesOf(portfolio, symbol) + shares } })
---
+++ src/misc/portfolio.test.js
@@ -0,0 +1,113 @@
+import { expect } from 'chai'
+import sinon from 'sinon'
+import * as Portfolio from './portfolio'
+
+const Bayer = 'BAYN'
+const BayerPrice = 42
+const IBM = 'IBM'
+const IBMPrice = 110
+
+describe('a portfolio', () => {
+  let portfolio
+
+  beforeEach(() => {
+    portfolio = Portfolio.createPortfolio()
+  })
+
+  it('is empty when created', () => {
+    expect(Portfolio.empty(portfolio)).to.be.true
+  })
+
+  it('is not empty after purchase', () => {
+    portfolio = Portfolio.purchase(portfolio, Bayer, 10)
+
+    expect(Portfolio.empty(portfolio)).to.be.false
+  })
+
+  it('has size 0 when created', () => {
+    expect(Portfolio.size(portfolio)).to.equal(0)
+  })
+
+  it('increases size after purchase', () => {
+    portfolio = Portfolio.purchase(portfolio, Bayer, 10)
+
+    expect(Portfolio.size(portfolio)).to.equal(1)
+  })
+
+  it('increments size with new symbol purchase', () => {
+    portfolio = Portfolio.purchase(portfolio, Bayer, 1)
+    portfolio = Portfolio.purchase(portfolio, 'IBM', 2)
+
+    expect(Portfolio.size(portfolio)).to.equal(2)
+  })
+
+  it('does not increment the size with duplicate symbol purchase', () => {
+    portfolio = Portfolio.purchase(portfolio, Bayer, 1)
+    portfolio = Portfolio.purchase(portfolio, Bayer, 1)
+
+    expect(Portfolio.size(portfolio)).to.equal(1)
+  })
+
+  it('answers 0 sharesOf for symbol not purchased', () => {
+    expect(Portfolio.sharesOf(portfolio, Bayer)).to.equal(0)
+  })
+
+  it('answers the number of sharesOf for a purchase', () => {
+    portfolio = Portfolio.purchase(portfolio, Bayer, 42)
+
+    expect(Portfolio.sharesOf(portfolio, Bayer)).to.equal(42)
+  })
+
+  it('does not alter passed state on purchase', () => {
+    Portfolio.purchase(portfolio, Bayer, 23)
+
+    expect(Portfolio.sharesOf(portfolio, Bayer)).to.equal(0)
+  })
+
+  it('accumulates shares for multiple purchases', () => {
+    portfolio = Portfolio.purchase(portfolio, Bayer, 23)
+    portfolio = Portfolio.purchase(portfolio, Bayer, 19)
+
+    expect(Portfolio.sharesOf(portfolio, Bayer)).to.equal(42)
+  })
+
+  describe('portfolio value', function () {
+    const lookup = sinon.stub()
+    lookup.withArgs(IBM).returns(IBMPrice)
+    lookup.withArgs(Bayer).returns(BayerPrice)
+
+    let prodLookup
+
+    beforeEach(function () {
+      prodLookup = Portfolio.symbolLookup
+      Portfolio.symbolLookupStub(lookup)
+    })
+
+    afterEach(function () {
+      Portfolio.symbolLookupStub(prodLookup)
+    })
+
+    it('is 0 when created', function () {
+      expect(Portfolio.value(portfolio)).to.equal(0)
+    })
+
+    it('is symbol price for single-share purchase', () => {
+      portfolio = Portfolio.purchase(portfolio, IBM, 1)
+
+      expect(Portfolio.value(portfolio)).to.equal(IBMPrice)
+    })
+
+    it('multiplies share price by number of shares', () => {
+      portfolio = Portfolio.purchase(portfolio, IBM, 20)
+
+      expect(Portfolio.value(portfolio)).to.equal(20 * IBMPrice)
+    })
+
+    it('sums values for all symbols', () => {
+      portfolio = Portfolio.purchase(portfolio, IBM, 20)
+      portfolio = Portfolio.purchase(portfolio, Bayer, 10)
+
+      expect(Portfolio.value(portfolio)).to.equal(20 * IBMPrice + 10 * BayerPrice)
+    })
+  })
+})
