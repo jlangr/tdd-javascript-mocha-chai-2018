ba013af3cb773e952b50897e8f39b4c83a7edeaf
Jeff Langr <Jeff@Langrsoft.com>
2018-12-05 09:32:15 -0700

    horror name normalizer

--- src/misc/name-normalizer.js
+++ src/misc/name-normalizer.js
@@ -1,5 +1,32 @@
-const countOfChar = (s, c) => (s.match(new RegExp(c, 'g'))||[]).length;
+//const countOfChar = (s, c) => (s.match(new RegExp(c, 'g'))||[]).length;
 
-export const normalize = _ => {
-  return 'wha?'
+const lastName = name => partsOfName(name)[partsOfName(name).length - 1]
+
+const firstName = name => partsOfName(name)[0]
+
+const initial = name => {
+  if (name.length === 1) return ` ${name}`
+  return ` ${name[0]}.`
+}
+
+const hasMiddleNames = name => partsOfName(name).length < 3
+
+const middleNames = array => array.slice(1, -1)
+
+const optionalMiddleInitials = name => {
+  if (hasMiddleNames(name)) return ''
+
+  return middleNames(partsOfName(name))
+    .map(initial)
+    .join('')
+}
+
+const isMononym = name => partsOfName(name).length === 1
+
+const partsOfName = name => name.split(' ')
+
+export const normalize = name => {
+  name = name.trim()
+  if (isMononym(name)) return name
+  return `${lastName(name)}, ${firstName(name)}${optionalMiddleInitials(name)}`
 }
